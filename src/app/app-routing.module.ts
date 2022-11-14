3ae2bbbc8b64850ac2f39c2e96a3cc1c33126f2b
hluismoises <hluismoises@gmail.com>
2022-11-13 21:07:17 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArchivosComponent } from './archivos/archivos.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { DetalleProductoComponent } from './detalle-producto/detalle-producto.component';
 
-const routes: Routes = [];
+import { LandingGeneralComponent } from './landing-general/landing-general.component';
+import { ListaEmpresasComponent } from './lista-empresas/lista-empresas.component';
+import { LoginComponent } from './login/login.component';
+import { OrdenesComponent } from './ordenes/ordenes.component';
+import { PlanesComponent } from './planes/planes.component';
+import { ProductosComponent } from './productos/productos.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'archivos',
+    component: ArchivosComponent,
+  },
+  {
+    path: 'cart',
+    component: CarritoComponent,
+  },
+  {
+    path: 'landing',
+    component: LandingGeneralComponent,
+  },
+  {
+    path: 'detalle-producto',
+    component: DetalleProductoComponent,
+  },
+  {
+    path: 'lista-empresas',
+    component: ListaEmpresasComponent,
+  },
+  {
+    path: 'planes',
+    component: PlanesComponent,
+  },
+  {
+    path: 'productos',
+    component: ProductosComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'ordenes',
+    component: OrdenesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingGeneralComponent } from './landing-general/landing-general.component';
+import { ArchivosComponent } from './archivos/archivos.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { DetalleProductoComponent } from './detalle-producto/detalle-producto.component';
+import { ListaEmpresasComponent } from './lista-empresas/lista-empresas.component';
+import { OrdenesComponent } from './ordenes/ordenes.component';
+import { PlanesComponent } from './planes/planes.component';
+import { ProductosComponent } from './productos/productos.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingGeneralComponent,
+    ArchivosComponent,
+    CarritoComponent,
+    DetalleProductoComponent,
+    ListaEmpresasComponent,
+    OrdenesComponent,
+    PlanesComponent,
+    ProductosComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
